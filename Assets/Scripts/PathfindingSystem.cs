f1daa145b20af3e7631bd40dea262589e358843a
SAJI J <saji290904@gmail.com>
2024-12-04 22:43:58 +0530

    Comments Updated On pathfinding

--- Assets/Scripts/PathfindingSystem.cs
+++ Assets/Scripts/PathfindingSystem.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PathfindingSystem : MonoBehaviour
 {
     [SerializeField] private GridCreator gridCreator;
 
+    // Find the path from the start position to the target position
     public List<GridTile> FindPath(Vector3Int startPos, Vector3Int targetPos)
     {
         var startTile = GetTileFromGridPosition(startPos);
@@ -13,13 +16,16 @@ public class PathfindingSystem : MonoBehaviour
         if (startTile == null || targetTile == null || !targetTile.IsWalkable)
             return null;
 
+        
         var openSet = new List<GridTile>();
         var closedSet = new HashSet<GridTile>();
         var nodeData = new Dictionary<GridTile, PathNode>();
 
+        // Add the start tile to the open set
         openSet.Add(startTile);
         nodeData[startTile] = new PathNode { gCost = 0, hCost = GetDistance(startTile, targetTile) };
 
+        // Loop through the open set until it is empty
         while (openSet.Count > 0)
         {
             var currentTile = GetLowestFCostTile(openSet, nodeData);
@@ -30,6 +36,7 @@ public class PathfindingSystem : MonoBehaviour
             openSet.Remove(currentTile);
             closedSet.Add(currentTile);
 
+            // Loop through the neighbors of the current tile
             foreach (var neighbor in gridCreator.GetNeighbours(currentTile))
             {
                 if (!neighbor.IsWalkable || closedSet.Contains(neighbor))
@@ -37,9 +44,11 @@ public class PathfindingSystem : MonoBehaviour
 
                 var newGCost = nodeData[currentTile].gCost + GetDistance(currentTile, neighbor);
 
+                // If the neighbor is not in the open set, add it
                 if (!nodeData.ContainsKey(neighbor))
                     nodeData[neighbor] = new PathNode();
 
+                // Update the gCost, hCost, and parent tile if the new gCost is less than the current gCost
                 if (newGCost < nodeData[neighbor].gCost || !openSet.Contains(neighbor))
                 {
                     nodeData[neighbor].gCost = newGCost;
@@ -56,6 +65,7 @@ public class PathfindingSystem : MonoBehaviour
     }
 
 
+    // Get the tile with the lowest F cost from the open set
     private GridTile GetLowestFCostTile(List<GridTile> openSet, Dictionary<GridTile, PathNode> nodeData)
     {
         var lowestTile = openSet[0];
@@ -63,6 +73,7 @@ public class PathfindingSystem : MonoBehaviour
 
         for (var i = 1; i < openSet.Count; i++)
         {
+            // Compare the F cost of the current tile with the lowest F cost
             var fCost = nodeData[openSet[i]].FCost;
             if (fCost < lowestFCost ||
                 (fCost == lowestFCost && nodeData[openSet[i]].hCost < nodeData[lowestTile].hCost))
@@ -75,11 +86,13 @@ public class PathfindingSystem : MonoBehaviour
         return lowestTile;
     }
 
+    // Retrace the path from the end tile to the start tile
     private List<GridTile> RetracePath(Dictionary<GridTile, PathNode> nodeData, GridTile startTile, GridTile endTile)
     {
         var path = new List<GridTile>();
         var currentTile = endTile;
 
+        // Loop through the parent tiles until the start tile is reached
         while (currentTile != startTile)
         {
             path.Add(currentTile);
@@ -90,14 +103,17 @@ public class PathfindingSystem : MonoBehaviour
         return path;
     }
 
+    // Get the distance between two tiles
     private int GetDistance(GridTile tileA, GridTile tileB)
     {
         var distX = Mathf.Abs(tileA.GridX - tileB.GridX);
         var distZ = Mathf.Abs(tileA.GridZ - tileB.GridZ);
 
+        // Calculate the distance using the diagonal distance formula ~ taken it from site: https://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
         return distX > distZ ? 14 * distZ + 10 * (distX - distZ) : 14 * distX + 10 * (distZ - distX);
     }
 
+    // Get the tile from the grid position
     public GridTile GetTileFromGridPosition(Vector3Int gridPosition)
     {
         var x = gridPosition.x;
@@ -108,6 +124,7 @@ public class PathfindingSystem : MonoBehaviour
         return null;
     }
 
+    //PathNode class to store the gCost, hCost, and parent tile
     private class PathNode
     {
         public int gCost = int.MaxValue;
